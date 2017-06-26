782a29d9bf8a739f3df95a9873c9b6bcad7f3e8e
mlovell82 <mlovell@kent.edu>
2017-06-26 15:17:28 -0400

    Create figuresInput.cpp

---
+++ cs1/lab5/figures/figuresInput.cpp
@@ -0,0 +1,33 @@
+// This program
+//Michael Lovell
+// 9/27/2015
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "1. square" << endl;
+	cout << "2. bottom left triangle" << endl;
+	cout << "3. top right triangle" << endl;
+	cout << "select figure:" << endl;
+	int figure;
+	cin >> figure;
+	cout << "select size:" << endl;
+	int size;
+	cin >> size;
+
+	if (figure == 1) {
+
+	}
+	else if (figure == 2) {
+
+	}
+	else if (figure == 3) {
+
+	}
+	else {
+		break;
+	}
+
+}
